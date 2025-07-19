3fb972cd18235e3c30d57fe3de0a4cb0daaf770e
Yevhen Tarasenko <yevhen.tarasenko1995@gmail.com>
2025-07-19 18:23:01 +0300

    test: Add valid BVA tests to createAccount

--- pageObjects/pages/CreateAccountPage.ts
+++ pageObjects/pages/CreateAccountPage.ts
@@ -11,20 +11,44 @@ export class CreateAccountPage extends BasePage {
     private readonly passwordConfirmField = this.page.locator("#password-confirmation");
     private readonly createAnAccountBtn = this.page.getByRole("button", { name: "Create an Account" });
     private readonly thankYouForRegisteringBlock = this.page.locator(".page.messages");
+    private readonly firstNameError = this.page.locator("#firstname-error");
+    private readonly lastNameError = this.page.locator("#lastname-error");
+    private readonly emailError = this.page.locator("#email_address-error");
+    private readonly passwordError = this.page.locator("#password-error");
+    private readonly passwordConfirmError = this.page.locator("#password-confirmation-error");
 
     async openCreateAccountPagePage() {
         await this.page.goto("/customer/account/create/");
         await expect(this.pageTitle).toHaveText("Create New Customer Account");
     }
 
-    async fillDataAndCreateAccountPage(user: User) {
+    async fillCreateAccountForm(user: User) {
         await this.firstNameField.fill(user.firstName);
         await this.lastNameField.fill(user.lastName);
         await this.emailField.fill(user.email);
         await this.passwordField.fill(user.password);
         await this.passwordConfirmField.fill(user.password);
-        await this.createAnAccountBtn.click();
+    }
 
+    async clickCreateAccountBtn() {
+        await this.createAnAccountBtn.click();
+    }
+
+    async expectUserIsRegistered() {
         await expect(this.thankYouForRegisteringBlock).toBeVisible();
+    }
+
+    async createValidUser(user: User) {
+        await this.fillCreateAccountForm(user);
+        await this.clickCreateAccountBtn();
+        await this.expectUserIsRegistered();
+    }
+
+    async expectRequiredFieldErrors() {
+        await expect(this.firstNameError).toHaveText("This is a required field.");
+        await expect(this.lastNameError).toHaveText("This is a required field.");
+        await expect(this.emailError).toHaveText("This is a required field.");
+        await expect(this.passwordError).toHaveText("This is a required field.");
+        await expect(this.passwordConfirmError).toHaveText("This is a required field.");
     }
 }
--- tests/ui/createAccount.spec.ts
+++ tests/ui/createAccount.spec.ts
@@ -1,10 +1,30 @@
 import { test } from "../../fixtures/fixturePageAccess";
 import { TestUserFactory } from "../../helpers/testDataGeneration";
 
-// generate user data
-let user = TestUserFactory.generateUser();
+test.describe("Create Account", () => {
+    test.beforeEach(async ({ app }) => {
+        await app.CreateAccountPage.openCreateAccountPagePage();
+    });
 
-test("Create a new account", async ({ app }) => {
-    await app.CreateAccountPage.openCreateAccountPagePage();
-    await app.CreateAccountPage.fillDataAndCreateAccountPage(user);
+    test("Create a new account", async ({ app }) => {
+        await app.CreateAccountPage.createValidUser(TestUserFactory.generateValidUser());
+    });
+
+    test("Create a new account with min number of symbols", async ({ app }) => {
+        await app.CreateAccountPage.fillCreateAccountForm(TestUserFactory.generateUserWithMinSymbols());
+        await app.CreateAccountPage.clickCreateAccountBtn();
+        await app.CreateAccountPage.expectUserIsRegistered();
+    });
+
+    test("Create a new account with max number of symbols", async ({ app }) => {
+        await app.CreateAccountPage.fillCreateAccountForm(TestUserFactory.generateUserWithMaxSymbols());
+        await app.CreateAccountPage.clickCreateAccountBtn();
+        await app.CreateAccountPage.expectUserIsRegistered();
+    });
+
+    test("Empty fields", async ({ app }) => {
+        await app.CreateAccountPage.fillCreateAccountForm(TestUserFactory.emptyUser());
+        await app.CreateAccountPage.clickCreateAccountBtn();
+        await app.CreateAccountPage.expectRequiredFieldErrors();
+    });
 });
